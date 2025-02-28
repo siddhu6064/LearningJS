f2c2395757783f60b35119e5e4047c5bfb2a8fea
siddhu6064 <siddhu.6064@gmail.com>
2025-02-28 14:13:51 -0500

    control flow

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -43,3 +43,7 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
